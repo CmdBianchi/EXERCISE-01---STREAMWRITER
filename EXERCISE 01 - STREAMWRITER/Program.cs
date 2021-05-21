b4412f2d6894132e12caf9388c24af465c11dc12
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-21 16:58:16 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - STREAMWRITER/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace EXERCISE_01___STREAMWRITER {
+    //-----> START
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath)) { 
+                    foreach(string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }               
+            }
+            catch(IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
